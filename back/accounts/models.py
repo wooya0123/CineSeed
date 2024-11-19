fe8075ac9d87a32352007f35041a5e0f5a362a1e
최현우 <aduma489@gmail.com>
2024-11-20 01:24:42 +0900

    feat: '유저 모델링'

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # username = models.CharField(max_length=100)
-    # password = models.CharField(max_length=100)
     class Role(models.TextChoices):
+        UNDEFINED = 'UN', 'Undefined'
         DIRECTOR = 'DI', 'Director'
         ACTOR = 'AC', 'Actor'
         STAFF = 'ST', 'Staff'
-        UNDEFINED = 'UN', 'Undefined'
 
     nickname = models.CharField(max_length=100)
     role = models.CharField(
@@ -20,6 +18,4 @@ class User(AbstractUser):
     introduction = models.TextField(blank=True)
     profile_image = models.ImageField(blank=True, upload_to='images/')
     instagram = models.URLField(blank=True)
-    email = models.EmailField(blank=True)
     etc = models.CharField(max_length=250, blank=True)
-
