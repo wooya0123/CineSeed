0563c512ed2c77a7666e7230f6dade94b90eaf06
nahyun <irene.k2547@gmail.com>
2024-11-19 23:54:50 +0900

    feat: 'movie 모델 설정'

--- back/movies/models.py
+++ back/movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, upload_to='images/')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    movie_introduction = models.TextField()
+    team_introduction = models.TextField()
+    budget_plan = models.TextField()
