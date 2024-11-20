c3f29ebc0f0960d01e001e62b7f95e9a51f11736
wooya0123 <aduma489@gmail.com>
2024-11-20 17:00:14 +0900

    feat: '유저 모델 수정'

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import Movie, Genre
 
 class User(AbstractUser):
     class Role(models.TextChoices):
@@ -8,14 +9,42 @@ class User(AbstractUser):
         ACTOR = 'AC', 'Actor'
         STAFF = 'ST', 'Staff'
 
+    genre = models.ForeignKey(Genre)          # 장르가 삭제되도 user는 삭제되면 안 됨  
     nickname = models.CharField(max_length=100)
     role = models.CharField(
         max_length=2,
         choices=Role.choices,
         default=Role.UNDEFINED
     )
-    preference = models.CharField(max_length=250, blank=True)
     introduction = models.TextField(blank=True)
     profile_image = models.ImageField(blank=True, upload_to='images/')
     instagram = models.URLField(blank=True)
     etc = models.CharField(max_length=250, blank=True)
+    cash = models.IntegerField(default=1000000)
+    title = models.CharField(max_length=100)
+    like_movies = models.ManyToManyField(Movie, through='LikeMovie', related_name='like_users')
+    fund_movies = models.ManyToManyField(Movie, through='FundMovie', related_name='fund_users')
+    apply_movies = models.ManyToManyField(Movie, through='ApplyMovie', related_name='apply_users')
+
+class LikeMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'like_movies'
+
+class FundMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    class Meta:
+        db_table = 'fund_movies'
+
+class ApplyMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    application = models.TextField()
+
+    class Meta:
+        db_table = 'apply_movies'
+
