67c8c3e49e4bac3916cafead13624673504c28e7
nahyun <irene.k2547@gmail.com>
2024-11-20 16:59:38 +0900

    feat: 'movie 모델 수정'

--- back/movies/models.py
+++ back/movies/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.conf import settings
 
 class Genre(models.Model):
-    code = models.IntegerField()
-    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
 
 class Movie(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     image = models.ImageField(blank=True, upload_to='images/')
     start_date = models.DateField()
@@ -14,25 +15,17 @@ class Movie(models.Model):
     movie_introduction = models.TextField()
     team_introduction = models.TextField()
     budget_plan = models.TextField()
-    genres = models.ManyToManyField(Genre, through='MovieAndGenre',related_name='movies')
+    is_appliable = models.BooleanField()
 
 class GameMovie(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     image = models.ImageField(blank=True, upload_to='images/')
-    genres = models.ManyToManyField(Genre, through='GameMovieAndGenre',related_name='game_movies')
 
-# movie - genre 중개테이블
-class MovieAndGenre(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'movies_and_genres'
-
-# gamemovie - genre 중개테이블
-class GameMovieAndGenre(models.Model):
-    game_movie = models.ForeignKey(GameMovie, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'game_movies_and_genres'
+class GameQuestion(models.Model):
+    type = models.CharField(max_length=100)
+    question1 = models.CharField(max_length=250)
+    question2 = models.CharField(max_length=250)
+    question3 = models.CharField(max_length=250)
+    question4 = models.CharField(max_length=250)
+    question5 = models.CharField(max_length=250)
