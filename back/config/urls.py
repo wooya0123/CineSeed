7ab889f8cfc3920dc581ee8dbeaa3649395b2a6c
nahyun <irene.k2547@gmail.com>
2024-11-20 17:27:05 +0900

    feat: 'config url 추가'

--- back/config/urls.py
+++ back/config/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('dj_rest_auth.urls')),
+    path('api/v1/', include('movies.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
