f4a78f552602012b25c203dc8e8ffebba6f594e9
wooya0123 <aduma489@gmail.com>
2024-11-21 11:58:19 +0900

    feat: 'movie 모델 수정, 시리얼라이저 추가'

--- back/movies/serializers.py
+++ back/movies/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
 
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
+        exclude = ('movie_introduction', 'team_introduction', 'budget_plan',)
+        read_only_fields = ('user', 'genre',)
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
         fields = '__all__'
         read_only_fields = ('user', 'genre',)
+
+class MovieCreateSerializer(serializers.ModelSerializer):
+    # 외래키인 genre를 사용자가 입력할 수 있도록
+    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        read_only_fields = ('user',)
+
+class MovieUpdateSerializer(serializers.ModelSerializer):
+    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all())
+
+    class Meta:
+        model = Movie
+        exclude = ('start_date', 'end_date',)
+        read_only_fields = ('user',)
