5ad368f208462c45f4ea975f510d6cfba3548862
wooya0123 <aduma489@gmail.com>
2024-11-21 10:43:20 +0900

    feat: '영화 조회 시리얼라이저 추가'

--- back/movies/views.py
+++ back/movies/views.py
@@ -6,9 +6,11 @@ from .models import Movie
 
 
 # 전체 db 대상
+@api_view(['GET', 'POST'])
 def movie_list(request):
     # 데이터 조회
-
+    if request.method == 'GET':
+        movies = Movie.objects.all()
 
     # 데이터 전송, db 수정
     pass
