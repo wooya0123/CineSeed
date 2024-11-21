772c0f1ce0981810ede508eb600f64c58e923459
wooya0123 <aduma489@gmail.com>
2024-11-21 10:10:55 +0900

    feat: 'url 설정'

--- back/movies/urls.py
+++ back/movies/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('fundings/', views.funding_list),
+    path('fundings/detail/', views.funding_detail),
 ]
--- back/movies/views.py
+++ back/movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def funding_list(request):
+    pass
+
+def funding_detail(request):
+    funding_id = request.GET.get('필드명')
