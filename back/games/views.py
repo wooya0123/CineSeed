9db8337cf538a1856d76f08019fdd6cb561f0957
nahyun <irene.k2547@gmail.com>
2024-11-21 18:00:01 +0900

    feat: '게임 알고리즘 구현'

--- back/games/views.py
+++ back/games/views.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import GameQuestion
-from movies.models import Genre
+from .models import GameQuestion, GameMovie
+from movies.models import Genre, Movie
 from .serializers import GameQuestionSerializer
 
 # Permission Decorators
@@ -14,6 +14,7 @@ from rest_framework.permissions import IsAuthenticated
 
 # game
 import random
+import requests
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])  # 로그인한 사용자만 게임을 플레이할 수 있음
@@ -54,6 +55,7 @@ def game_title(request):
     request.user.title = title
     # request.user에 genre 객체 값 저장
     request.user.genre = user_genre
+    request.user.save()
 
     # STEP 2. 게임 영화 DB 넘겨주기
 
@@ -101,3 +103,63 @@ def find_category(category_code):
     }
 
     return code_dict.get(category_code)
+
+@api_view(['POST'])
+def set_game_movie_data(request):
+    # TMDB의 genre_id와 우리 장르 분류 관계를 저장한 dict 만들기
+    convert_genre = {
+        '28' : 'H', # 액션 H
+        '12' : 'H', # 모험 H
+        '16' : 'G', # 애니메이션 G
+        '35' : 'E', # 코미디 E
+        '80' : 'H', # 범죄 H
+        '99' : 'C', # 다큐멘터리 C
+        '18' : 'D', # 드라마 D
+        '10751' : 'E', # 가족 E
+        '14' : 'G', # 판타지 G
+        '36' : 'C', # 역사 C
+        '27' : 'F', # 공포 F
+        '10402' : 'B', # 음악 B
+        '9648' : 'F', # 미스터리 F
+        '10749' : 'A', # 로맨스 A
+        '878' : 'G', # SF G
+        '10770' : 'D', # TV 영화 D
+        '53' : 'F', # 스릴러 F
+        '10752' : 'H', # 전쟁 H
+        '37' : 'H', # 서부 H
+    }
+    # 영화 이미지 url 앞에 부분
+    base_url = "https://image.tmdb.org/t/p/original"
+    
+    # api에 요청해서 정보 가져오기
+    api_key = settings.API_KEY
+    url = 'https://api.themoviedb.org/3/movie/top_rated'
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    params = {
+        "api_key": api_key,
+        "language": 'ko-KR',
+        "region": 'KR',
+        "page": 1,
+    }
+    response = requests.get(url, headers=headers, params=params).json()
+
+    # response에 들어있는 영화를 순회
+    for movie in response.get('results'):
+        # GameMovie에 들어갈 정보들을 변수에 저장
+        title = movie.get('title')
+        poster_path = movie.get('poster_path')
+        genre_id = movie.get('genre_ids')[0]
+
+        # genre_id를 '우리가 정의한 장르'와 매칭시키기
+        genre_id = convert_genre.get(str(genre_id))
+
+
+        # DB에 저장
+        image_url = f"{base_url}{poster_path}"
+        new_movie = GameMovie.objects.create(title=title, image=image_url)
+        new_movie.genre = Genre.objects.get(code=genre_id)
+        new_movie.save()
+
+    return Response(response)
