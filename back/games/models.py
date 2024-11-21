8b2b4c69226aeba8396ea7631d484d13822cfb4a
nahyun <irene.k2547@gmail.com>
2024-11-21 10:24:38 +0900

    feat: 'games 앱 분리'

---
+++ back/games/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from movies.models import Genre
+
+# Create your models here.
+class GameMovie(models.Model):
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+class GameQuestion(models.Model):
+    type = models.CharField(max_length=100)
+    question1_text = models.CharField(max_length=250)
+    question1_a = models.CharField(max_length=250)
+    question1_b = models.CharField(max_length=250)
+    question2_text = models.CharField(max_length=250)
+    question2_a = models.CharField(max_length=250)
+    question2_b = models.CharField(max_length=250)
+    question3_text = models.CharField(max_length=250)
+    question3_a = models.CharField(max_length=250)
+    question3_b = models.CharField(max_length=250)
+    question4_text = models.CharField(max_length=250)
+    question4_a = models.CharField(max_length=250)
+    question4_b = models.CharField(max_length=250)
+    question5_text = models.CharField(max_length=250)
+    question5_a = models.CharField(max_length=250)
+    question5_b = models.CharField(max_length=250)
