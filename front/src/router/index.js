9cc8bdc505fedfd074077c2c8a068738739c7887
최현우 <aduma489@gmail.com>
2024-11-24 05:30:55 +0900

    feat: '네비게이션 가드 추가'

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,8 +1,10 @@
 import LogInView from '@/views/LogInView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import BaseHomeView from '@/views/BaseHomeView.vue'
+import BaseProfileView from '@/views/BaseProfileView.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAccountStore } from '@/stores/account'
 
 
 const router = createRouter({
@@ -23,7 +25,27 @@ const router = createRouter({
       name: 'login',
       component: LogInView
     },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: BaseProfileView,
+      meta: { requiresAuth: true } // 인증이 필요한 라우트에 메타 필드 추가
+    },
   ],
+})
+
+// 네비게이션 가드 추가
+router.beforeEach((to, from, next) => {
+  const accountStore = useAccountStore()
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!accountStore.isLogIn) {
+      next({ name: 'login' }) // 로그인 페이지로 리디렉션
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
