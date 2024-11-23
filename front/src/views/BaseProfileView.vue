a6d8e05ccf6244a18cd950fa719dc00dfdaa42e1
최현우 <aduma489@gmail.com>
2024-11-24 05:33:47 +0900

    feat: '마이페이지 구성'

--- front/src/views/BaseProfileView.vue
+++ front/src/views/BaseProfileView.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-
+    {{ account.profile}}
   </div>
 </template>
 
 <script setup>
+import MovieCarousel from '@/components/MovieCarousel.vue';
 
+import { useAccountStore } from '@/stores/account';
+import { onMounted } from 'vue';
+
+const account = useAccountStore()
+
+onMounted(() => {
+  console.log(account.profile)
+  account.myPage()
+})
 </script>
 
 <style scoped>
