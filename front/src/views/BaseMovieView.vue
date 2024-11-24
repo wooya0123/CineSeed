feab5a2ddf6ca8b78f355cc1f08bb6113a293584
최현우 <aduma489@gmail.com>
2024-11-25 05:55:37 +0900

    feat: '홈 화면 변경'

---
+++ front/src/views/BaseMovieView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <RouterLink
+      v-for="movie in movies"
+      :key="movie.id"
+      :to="{ name: 'movieDetail', params: { id : movie.id } }"
+      >
+      <MovieCard :movie="movie"/>
+    </RouterLink>
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import MovieCard from '@/components/MovieCard.vue';
+import axios from 'axios';
+
+import { onMounted, ref } from 'vue';
+import { useAccountStore } from '@/stores/account';
+
+const account = useAccountStore()
+const movies = ref(null)
+
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: `${account.API_URL}/api/v1/movie/`
+  })
+  .then((res) => {
+    movies.value = res.data
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+})
+
+</script>
+
+<style scoped>
+
+</style>
