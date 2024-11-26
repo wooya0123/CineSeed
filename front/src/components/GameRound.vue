9572c5a41d9b58affa6e80afb7f179df51545e26
nahyun <irene.k2547@gmail.com>
2024-11-26 14:12:11 +0900

    design: 'GameRound 제작'

--- front/src/components/GameRound.vue
+++ front/src/components/GameRound.vue
@@ -1,21 +1,52 @@
 <template>
-    <div>
+    <div class="container">
         <div v-if="questions && questionIndex < questions.length">
-            <h2>{{ questions[questionIndex].text }}</h2>
-            <button @click="selectAnswer('a')">{{  questions[questionIndex].a }}</button>
-            <button @click="selectAnswer('b')">{{  questions[questionIndex].b }}</button>
+            <div class="question bg-white primary">
+                <h3>{{ questions[questionIndex].text }}</h3>
+            </div>
+
+            <div class="answer gray6">
+                <div class="answer-card-sm"
+                    @mouseover="isHoveredA = true" 
+                    @mouseleave="isHoveredA = false"
+                >
+                    <div class="answer-card-title" :class="{ 'hovered': isHoveredA }">A</div>
+                    <div class="answer-card-text" @click="selectAnswer('a')">{{  questions[questionIndex].a }}</div>
+                </div>
+                <div class="answer-card-sm"
+                    @mouseover="isHoveredB = true" 
+                    @mouseleave="isHoveredB = false"
+                >
+                    <div class="answer-card-title" :class="{ 'hovered': isHoveredB }">B</div>
+                    <div class="answer-card-text" @click="selectAnswer('b')">{{  questions[questionIndex].b }}</div>
+                </div>
+            </div>
         </div>
+
         <div v-if="movies && movieIndex < movies.length">
-            <h2>두 영화 중 더 선호하는 영화를 선택해주세요</h2>
-            <div>
-                <label for="movie_a">{{ movies[movieIndex].title_a }}</label>
-                <img @click="selectMovie" :src="movies[movieIndex].source_a" alt="영화a" id="movie_a">
+            <div class="question bg-white primary">
+                <h3>두 영화 중 더 선호하는 영화를 선택해주세요</h3>
             </div>
-            <div>
-                <label for="movie_b">{{ movies[movieIndex].title_b }}</label>
-                <img @click="selectMovie" :src="movies[movieIndex].source_b" alt="영화b" id="movie_b">
+
+            <div v-show="isLoading" class="loading-spinner"></div>
+            <div v-show="!isLoading" class="answer gray6">
+                <div class="answer-card-lg"
+                    @mouseover="isHoveredA = true" 
+                    @mouseleave="isHoveredA = false"
+                >
+                    <div class="answer-card-title" :class="{ 'hovered': isHoveredA }">{{ movies[movieIndex].title_a }}</div>
+                    <img class="answer-card-movie" @click="selectMovie" :src="movies[movieIndex].source_a" alt="영화a" id="movie_a" @load="onImageLoadA">
+                </div>
+                <div class="answer-card-lg"
+                    @mouseover="isHoveredB = true" 
+                    @mouseleave="isHoveredB = false"
+                >
+                    <div class="answer-card-title" :class="{ 'hovered': isHoveredB }">{{ movies[movieIndex].title_b }}</div>
+                    <img class="answer-card-movie" @click="selectMovie" :src="movies[movieIndex].source_b" alt="영화b" id="movie_b" @load="onImageLoadB">
+                </div>
             </div>
         </div>
+
         <div v-if="movies && movieIndex >= movies.length">
             <h2>취향 분석이 완료되었습니다.</h2>
             <h3>{{ userTitle }}</h3>
@@ -39,10 +70,40 @@ const questionIndex = ref(0)    // 현재 보고 있는 문제가 몇 번인지 
 const movies = ref(null)        // 영화 리스트
 const movieIndex = ref(0)
 
+const isLoading = ref(true)
+const isLoadingA = ref(true) // A 영화 이미지 로딩 상태
+const isLoadingB = ref(true) // B 영화 이미지 로딩 상태
+
+// 이미지 로딩 완료 핸들러
+const onImageLoadA = () => {
+  isLoadingA.value = false
+  checkAllImagesLoaded()
+}
+
+const onImageLoadB = () => {
+  isLoadingB.value = false
+  checkAllImagesLoaded()
+}
+
+const checkAllImagesLoaded = () => {
+    if (!isLoadingA.value && !isLoadingB.value) {
+        isLoading.value = false
+    }
+}
+// movies 또는 movieIndex가 변경되면 로딩 상태 초기화
+watch(movieIndex, () => {
+  isLoadingA.value = true;
+  isLoadingB.value = true;
+  isLoading.value = true;
+});
+
 const router = useRouter()
 const accountStore = useAccountStore()
 
 const userTitle = ref(null)
+
+const isHoveredA = ref(false) // hover 상태를 추적하는 변수
+const isHoveredB = ref(false) // hover 상태를 추적하는 변수
 
 // 게임을 시작할 때, 게임 질문 가져오기
 onMounted(() => {
@@ -227,5 +288,104 @@ const goHome = function () {
 </script>
 
 <style scoped>
+.question {
+    width: 100%;
+    max-width: 962px; /* 최대 너비 제한 */
+    min-width: 516px;
+    height: 15vw;
+    max-height: 160px;
+    min-height: 140px;
+    border-radius: 20px;
+    white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+    text-align: center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.answer {
+    width: 100%;
+    max-width: 962px; /* 최대 너비 제한 */
+    min-width: 516px;
+    height: 20vw;
+    max-height: 180px;
+    min-height: 160px;
+    padding: 0px;
+    margin-top: 32px;
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-content: space-between; /* 아이템 간 간격을 동일하게 배치 */
+    gap: 16px;  /* 요소 간의 간격 설정 */
+}
+
+.answer-card-title {
+    width: 100%;
+    height: 32px;
+    border-top-left-radius: 10px;  /* 왼쪽 상단 코너 */
+    border-top-right-radius: 10px; /* 오른쪽 상단 코너 */
+    background-color: #D0D5DA;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Pretendard-Medium';
+}
+
+.answer-card-title.hovered {
+    background-color: #FB4CA1;
+    cursor: pointer;
+}
+
+.answer-card-sm {
+    flex: 1;
+    width: 100%;
+}
+
+.answer-card-text {
+    width: 100%;
+    height: 80px;
+    border-bottom-left-radius: 10px;  /* 왼쪽 상단 코너 */
+    border-bottom-right-radius: 10px; /* 오른쪽 상단 코너 */
+    background-color: #F5FAFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.answer-card-lg{
+    flex: 1;
+    width: 100%;
+}
+
+.answer-card-movie {
+    width: 100%;
+    max-height: 640px;
+}
+
+.loading-spinner {
+  text-align: center;
+  font-size: 16px;
+  color: #F5FAFF;
+}
+
+.loading-spinner::after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 20px;
+  height: 20px;
+  border: 3px solid #F5FAFF;
+  border-top: 3px solid #FB4CA1;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+  0% { transform: translate(-50%, -50%) rotate(0deg); }
+  100% { transform: translate(-50%, -50%) rotate(360deg); }
+}
 
 </style>
