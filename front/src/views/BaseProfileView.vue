bb9a970fcfff24859be1a16488a2ceae8294d472
nahyun <irene.k2547@gmail.com>
2024-11-27 05:42:53 +0900

    design: '지원서 보기 기능 및 css'

--- front/src/views/BaseProfileView.vue
+++ front/src/views/BaseProfileView.vue
@@ -17,16 +17,16 @@
             <h3>{{ nickname }}님</h3>
             <div class="contact">
               <!-- 인스타 정보가 있을 경우 -->
-              <a v-if="profileOwner.instagram" :href="profileOwner.instagram">인스타</a>
+              <a v-if="profileOwner.instagram" :href="profileOwner.instagram"><i class="bi bi-instagram"></i></a>
               <!-- etc 정보가 있을 경우 -->
               <div v-if="profileOwner.etc">
                 <!-- etc가 주소인 경우 -->
                 <span v-if="profileOwner.etc.includes('www')">
-                  <a :href="profileOwner.etc" target="_blank" rel="noopener noreferrer">연락처</a>
+                  <a :href="profileOwner.etc" target="_blank" rel="noopener noreferrer"><i class="bi bi-link"></i></a>
                 </span>
                 <!-- etc가 메일인 경우 -->
                 <span v-else @click="copyToClipboard(account.user.etc)" style="cursor: pointer; color: blue;">
-                  연락처 (클릭하여 복사)
+                  <i class="bi bi-envelope"></i>
                 </span>
               </div>
             </div>
@@ -39,7 +39,7 @@
     </div>
     
     <section class="movie-carousel">
-      <h3>{{ nickname }}님이 후원한 영화</h3>
+      <h2>{{ nickname }}님이 후원한 영화</h2>
       <MovieCarousel v-show="profileOwner.fund_movies.length" :movies="profileOwner.fund_movies"/>
       <div class="info gray2 bg-gray5" v-show="!profileOwner.fund_movies.length">
         아직 후원한 영화가 없어요
@@ -47,7 +47,7 @@
     </section>
     
     <section class="movie-carousel">
-      <h3>{{ nickname }}님이 좋아요한 영화</h3>
+      <h2>{{ nickname }}님이 좋아요한 영화</h2>
       <MovieCarousel v-show="profileOwner.like_movies.length" :movies="profileOwner.like_movies"/>
       <div class="info gray2 bg-gray5" v-show="!profileOwner.like_movies.length">
         아직 좋아요한 영화가 없어요
@@ -55,23 +55,22 @@
     </section>
 
     <section class="movie-carousel" v-if="profileOwner.role === '유저'">
-      <h3>혹시, 감독/배우/크루세요?</h3>
+      <h2>혹시, 감독/배우/크루세요?</h2>
       <p>그렇다면, 회원 정보 수정으로 펀딩에 참여하거나, 펀딩을 시작해보세요!</p>
     </section>
     
     <section class="movie-carousel" v-else-if="profileOwner.role === '감독'">
-      <h3>{{ nickname }}님의 영화</h3>
+      <div class="carousel-heading">
+        <h2 style="flex: 1;">{{ nickname }}님의 영화</h2>
+        <button class="btn-m bg-primary" v-if="profileOwner.id === account.user.id" @click="checkApplication">지원서 보기</button>
+      </div>
       <div class="info gray2 bg-gray5" v-show="!profileOwner.my_movie.length">
         아직 등록한 영화가 없어요
       </div>
       <div 
-        v-show="profileOwner.my_movie.length"
-        v-for="movie in profileOwner.my_movie"
-        :key="movie.id"
+        v-if="profileOwner.my_movie.length"
         >
-        <p>{{ movie.title }}</p>
-        <img class="movie_poster" :src="`${account.API_URL}${movie.image}`" alt="">
-        <p>{{ movie.apply_users }}</p>
+        <MovieCarousel :movies="profileOwner.my_movie"/>
       </div>
     </section>
     
@@ -82,11 +81,19 @@
         아직 지원한 영화가 없어요
       </div>
     </section>
+
+    <!-- 모달 컴포넌트 -->
+    <ApplicationModal
+      v-if="isModalVisible"
+      :ApplicationData="selectedApplicationData"
+      @close="isModalVisible = false"
+    />
   </div>
 </template>
 
 <script setup>
 import MovieCarousel from '@/components/MovieCarousel.vue';
+import ApplicationModal from '@/components/ApplicationModal.vue';
 
 import { useAccountStore } from '@/stores/account';
 import axios from 'axios';
@@ -102,13 +109,30 @@ const route = useRoute()
 const account = useAccountStore()
 const profileOwnerId = ref(route.params.id)
 const profileOwner = ref(null)
+const profileAPI = account.API_URL + '/api/v1/profile/' // 프로필 데이터 가져올 주소
+
+
+const loadProfile = () => {
+  axios({
+    method: 'get',
+    url: `${profileAPI}${profileOwnerId.value}/`,
+    headers: {
+      Authorization: `Token ${account.token}`,
+    },
+  })
+  .then((res) => {
+    profileOwner.value = res.data;
+  })
+  .catch((err) => {
+    console.log('프로필 불러오기 실패', err);
+  });
+};
 
 const nickname = ref(null)
-
 // 연락처 아이콘 클릭하면 클립보드에 복사
 const copyToClipboard = (text) => {
   navigator.clipboard.writeText(text).then(() => {
-    alert('클립보드에 복사되었습니다.')
+    alert('메일 주소가 클립보드에 복사되었습니다.')
   }).catch((err) => {
     console.error('클립보드 복사 실패:', err)
   })
@@ -116,25 +140,20 @@ const copyToClipboard = (text) => {
 
 const image = ref(null)
 const introduction = ref(true)
-const profileAPI = account.API_URL + '/api/v1/profile/' // 프로필 데이터 가져올 주소
 
 onMounted(() => {
-  // 이 프로필 주인 찾기
-  axios({
-    method: 'get',
-    url: `${profileAPI}${profileOwnerId.value}/`,
-    headers: {
-      'Authorization': `Token ${account.token}`,
-    },
-  })
-    .then((res) => {
-      profileOwner.value = res.data
-      console.log(profileOwner.value);
-    })
-    .catch((err) => {
-      console.log('프로필 불러오기 실패', err)
-    })
+  loadProfile()
 })
+
+// 라우트 파라미터 변경 감지
+watch(
+  () => route.params.id,
+  (newId) => {
+    profileOwnerId.value = newId;
+    loadProfile(); // 새 ID에 대해 데이터를 다시 가져옴
+    isModalVisible.value = false
+  }
+);
 
 watch(profileOwner,
   (newVal) => {
@@ -163,6 +182,22 @@ watch(profileOwner,
 const goEditProfile = function () {
   router.push({ name : 'profileEdit' })
 }
+
+
+const isModalVisible = ref(false); // 모달 표시 여부
+const selectedApplicationData = ref(null); // 전달할 데이터
+
+// 모달 표시 함수
+const checkApplication = () => {
+  // apply_users가 비어 있지 않은 데이터만 변환
+  selectedApplicationData.value = profileOwner.value.my_movie
+    .filter((movie) => movie.apply_users.length > 0) // 지원자가 있는 영화만 필터링
+    .map((movie) => ({
+      movieTitle: movie.title,
+      applicants: movie.apply_users,
+    }));
+  isModalVisible.value = true; // 모달 표시
+};
 </script>
 
 <style scoped>
@@ -208,6 +243,13 @@ const goEditProfile = function () {
   border-radius: 20px;
 }
 
+.contact {
+  padding: 0px 12px;
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: center;
+}
 
 .movie_poster {
   width: 30%;
@@ -238,5 +280,15 @@ const goEditProfile = function () {
 .edit-btn:hover {
   color: #FB4CA1;
   cursor: pointer;
+}
+
+i {
+  color: #FB4CA1;
+}
+
+.carousel-heading {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 8px;
 }
 </style>
